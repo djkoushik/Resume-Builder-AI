8288cb5ed3ddc69614ccce0ffba28b64d7512a3d
djkoushik <42695169+djkoushik@users.noreply.github.com>
2025-11-13 19:24:39 +0530

    update to deploy to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,18 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+          input: 'index.html',
+          output: {
+            entryFileNames: '[name]-[hash].js',
+            chunkFileNames: '[name]-[hash].js',
+            assetFileNames: '[name]-[hash].[ext]'
+          }
+        }
       }
     };
 });
